ec4d0429f0620c9ddb860844ce2f344c269adbda
tholmes101 <piece4ulmind@gmail.com>
2026-02-27 01:39:05 -0500

    Update added

---
+++ src/Context/ShopContext.jsx
@@ -0,0 +1,66 @@
+import React, { createContext, useState } from "react";
+// import all_product from "../Components/Assets/all_product";
+import data_product from '../Components/Assets/data'
+
+export const ShopContext = createContext(null);
+
+const getDefaultCart = () => {
+  let cart = {};
+  for (let index = 0; index < data_product.length + 1; index++) {
+    cart[index] = 0;
+  }
+  return cart;
+};
+
+const ShopContextProvider = (props) => {
+  const [cartItems, setCartItems] = useState(getDefaultCart);
+
+  const addToCart = (itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
+    console.log(cartItems);
+  };
+
+  const removeFromCart = (itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
+  };
+
+  const getTotalCartAmount = () => {
+    let totalAmount = 0;
+    for (const item in cartItems) {
+      if (cartItems[item] > 0) {
+        let itemInfo = data_product.find(
+          (product) => product.id === Number(item)
+        );
+        totalAmount += itemInfo.new_price * cartItems[item];
+      }
+    }
+    return totalAmount;
+  };
+
+  const getTotalCartItems = () => {
+    let totalItem = 0;
+    for (const item in cartItems) {
+      if (cartItems[item] > 0) {
+        totalItem += cartItems[item];
+      }
+    }
+    return totalItem;
+  }
+
+  const contextValue = {
+    data_product,
+    cartItems,
+    addToCart,
+    removeFromCart,
+    getTotalCartAmount,
+    getTotalCartItems,
+  };
+
+  return (
+    <ShopContext.Provider value={contextValue}>
+      {props.children}
+    </ShopContext.Provider>
+  );
+};
+
+export default ShopContextProvider;
---
+++ src/Pages/Shop.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Hero from '../Components/Hero/Hero'
+import Products from '../Components/Products/Products'
+
+const Shop = () => {
+  return (
+    <div>
+        <Hero />
+        <Products />
+    </div>
+  )
+}
+
+export default Shop
